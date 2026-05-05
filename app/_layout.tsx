7686e785c57ffc5dd6438cd26016e188f0fe908a
viniciusAhenrique <andradevinihenrique@gmail.com>
2026-05-05 01:33:44 -0300

    Fix root route and Android app icon

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaProvider>
+  );
+}
